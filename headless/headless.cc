42213b8012a7ecc6abeb31e17cccfb54f473b8e7
Nestal Wan <me@nestal.net>
2017-09-18 12:44:20 +0800

    Added headless test driver

---
+++ headless/headless.cc
@@ -0,0 +1,41 @@
+/////////////////////////////////////////////////////////////////////////
+//
+// Copyright (C) 2017 Thomson Reuters Limited
+// This document contains information proprietary to Thomson Reuters Limited, and
+// may not be reproduced, disclosed or used in whole or in part without
+// the express written permission of Thomson Reuters Limited.
+//
+/////////////////////////////////////////////////////////////////////////
+
+#include <QtWebEngine/QtWebEngine>
+#include <QWebEnginePage>
+#include <QEventLoop>
+#include <QApplication>
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	QApplication app(argc, argv);
+	QtWebEngine::initialize();
+
+	QString filename = "output.pdf";
+	QWebEnginePage page;
+
+	app.connect(&page, &QWebEnginePage::loadFinished, [&page, &app, filename]()
+    {
+        page.printToPdf([&app, filename] (QByteArray ba)
+        {
+            QFile f(filename);
+            if (f.open(QIODevice::WriteOnly))
+            {
+                f.write(ba);
+                f.close();
+            } else {
+                qDebug() << "Error opening file for writing" << filename << f.errorString();
+            }
+            app.exit();
+        });
+    });
+    page.load({"https://google.com"});
+    return app.exec();
+}
