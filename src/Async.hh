65c15fdb03f80881121b86d0d04f2fc8602758be
Nestal Wan <me@nestal.net>
2017-10-02 00:54:52 +0800

    Ensure we call "func" before waiting for the callback future

--- src/Async.hh
+++ src/Async.hh
@@ -15,6 +15,7 @@
 #include "FunctorEvent.hh"
 
 #include <future>
+#include <thread>
 #include <QDebug>
 
 namespace wacrana {
@@ -23,13 +24,33 @@ template <typename T>
 class ThenableFuture
 {
 public:
+	ThenableFuture() = default;
+	
 	template <typename Func>
 	explicit ThenableFuture(Func&& func)
+	{
+		Start(std::forward<Func>(func));
+	}
+	
+	template <typename Func>
+	void Start(Func&& func)
 	{
 		auto future = m_promise.get_future();
 		std::thread{[func=std::forward<Func>(func), future=std::move(future)]() mutable
 		{
-			PostMain([result=func(), callback=future.get()]() mutable
+			// Run the function "func" in the spawned thread. The return value will be
+			// moved to the lambda closure to be used later.
+			auto result = func();
+			
+			// Wait for the main thread to send us the callback function, which will be
+			// called by the main thread to handle the return value of "func".
+			// The callback function is specified by Then().
+			auto callback = future.get();
+			
+			// Move the result of "func" and the callback from Then() to a lambda closure,
+			// and post it back to the main thread. The main thread will execute the
+			// lambda after it is received.
+			PostMain([result=std::move(result), callback=std::move(callback)]() mutable
 			{
 				callback(result);
 			});
