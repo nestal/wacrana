fc62b74060b545c208d5bef8840ec82af206c21d
Nestal Wan <me@nestal.net>
2017-10-02 15:44:30 +0800

    don't know if it works

--- src/async/Async.hh
+++ src/async/Async.hh
@@ -19,6 +19,54 @@
 namespace exe {
 
 template <typename T>
+class Future;
+
+template <typename T, typename Continuation>
+class NextPackage
+{
+public:
+	using ContResult = decltype(std::declval<Continuation&>()(std::declval<T>()));
+	
+	NextPackage(Continuation&& func, std::future<Token>&& token) : m_token{std::move(token)}, m_func{std::move(func)}
+	{
+	}
+	
+	template <typename Arg, typename Q=ContResult>
+	typename std::enable_if<!std::is_void<Q>::value>::type Continue(Arg&& val)
+	{
+		m_result.set_value(m_func(std::forward<Arg>(val)));
+	}
+	
+	template <typename Arg, typename Q=ContResult>
+	typename std::enable_if<std::is_void<Q>::value>::type Continue(Arg&& val)
+	{
+		m_func(std::forward<Arg>(val));
+		m_result.set_value();
+	}
+	
+	void Notify()
+	{
+		// notify next
+		if (m_token.wait_for(std::chrono::system_clock::duration::zero()) == std::future_status::ready)
+		{
+			auto t = m_token.get();
+			if (t.host)
+				t.host->Execute(t);
+		}
+	}
+
+	std::future<ContResult> ResultFuture()
+	{
+		return m_result.get_future();
+	}
+	
+private:
+	std::future<Token>          m_token;
+	std::promise<ContResult>    m_result;
+	Continuation                m_func;
+};
+
+template <typename T>
 class Future
 {
 public:
@@ -31,32 +79,18 @@ public:
 	}
 	
 	template <typename Func>
-	auto Then(Func&& continuation, Executor *host,
-		typename std::enable_if<!std::is_void<decltype(continuation(std::declval<T>()))>::value>::type* = 0)
+	auto Then(Func&& continuation, Executor *host)
 	{
-		using U = decltype(continuation(std::declval<T>()));
-		auto                next_result = std::make_shared<std::promise<U>>();
+		using Next = NextPackage<T, Func>;
+		
 		std::promise<Token> next_token;
+		auto next = std::make_shared<Next>(std::forward<Func>(continuation), next_token.get_future());
 		
-		auto next_future = next_result->get_future();
-		auto result      = m_shared_state.share();
-		
-		auto token      = host->Add([
-			result,
-			func        = std::forward<Func>(continuation),
-			next_result = std::move(next_result),
-			next_token  = next_token.get_future().share()
-		]() mutable
+		auto result     = m_shared_state.share();
+		auto token      = host->Add([result, next]
 		{
-			next_result->set_value(func(result.get()));
-			
-			// notify next
-			if (next_token.wait_for(std::chrono::system_clock::duration::zero()) == std::future_status::ready)
-			{
-				auto t = next_token.get();
-				if (t.host)
-					t.host->Execute(t);
-			}
+			next->Continue(result.get());
+			next->Notify();
 		});
 		
 		m_thenned = true;
@@ -66,46 +100,7 @@ public:
 		else
 			m_token.set_value(token);
 		
-		return Future<U>{std::move(next_future), std::move(next_token)};
-	}
-	
-	template <typename Func>
-	auto Then(Func&& continuation, Executor *host,
-		typename std::enable_if<std::is_void<decltype(continuation(std::declval<T>()))>::value>::type* = 0)
-	{
-		auto                next_result = std::make_shared<std::promise<void>>();
-		std::promise<Token> next_token;
-		
-		auto next_future = next_result->get_future();
-		auto result      = m_shared_state.share();
-		
-		auto token      = host->Add([
-			result,
-			func        = std::forward<Func>(continuation),
-			next_result = std::move(next_result),
-			next_token  = next_token.get_future().share()
-		]() mutable
-		{
-			func(result.get());
-			next_result->set_value();
-			
-			// notify next
-			if (next_token.wait_for(std::chrono::system_clock::duration::zero()) == std::future_status::ready)
-			{
-				auto t = next_token.get();
-				if (t.host)
-					t.host->Execute(t);
-			}
-		});
-		
-		m_thenned = true;
-		
-		if (result.wait_for(std::chrono::system_clock::duration::zero()) == std::future_status::ready)
-			host->Execute(token);
-		else
-			m_token.set_value(token);
-		
-		return Future<void>{std::move(next_future), std::move(next_token)};
+		return Future<typename Next::ContResult>{next->ResultFuture(), std::move(next_token)};
 	}
 	
 	Future(Future&& f) :
